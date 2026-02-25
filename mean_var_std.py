49583a2575254275fba1fbf23badfd6e7383b4d9
FrankTiako <ndengwef@gmail.com>
2026-02-25 04:34:13 +0100

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,1 +1,16 @@
+import numpy as np
 
+def calculate(digits_list):
+    if len(digits_list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    matrix = np.array(digits_list).reshape(3, 3)
+    dico = {
+        'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().tolist()],
+        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().tolist()],
+        'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().tolist()],
+        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().tolist()],
+        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().tolist()],
+        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().tolist()],
+    }
+    return dico
+print(calculate([2,6,2,8,4,0,1,5,7]))
